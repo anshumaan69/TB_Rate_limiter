29b5e6de370ff27b8f78821456ada6fb8fb68282
Anshumaan Mishra <anshumaan.1611@gmail.com>
2026-06-26 14:25:11 +0530

    feat: implement rate limiter logic with request window tracking and reset time calculation

---
+++ src/services/rateLimiterRouter.js
@@ -0,0 +1,23 @@
+import { prisma } from "../utils/prismaClient.js";
+import * as clientRepository from "../repositories/clientRepository.js";
+import * as tokenBucketService from "./tokenBucket/tokenBucketService.js";
+import * as slidingWindowService from "./slidingWindow/slidingWindowService.js";
+
+export const checkRequest = async (clientKey) => {
+    return prisma.$transaction(async (tx) => {
+        // 1. Client settings retrieve karein
+        const client = await clientRepository.findByKey(clientKey, tx);
+        if (!client) {
+            throw new Error(`Client not configured: ${clientKey}`);
+        }
+
+        // 2. Algorithm ke basis par route karein
+        if (client.algorithm === "token_bucket") {
+            return tokenBucketService.checkLimit(client.client_key, client.rate, client.burst, tx);
+        } else if (client.algorithm === "sliding_window") {
+            return slidingWindowService.checkLimit(client.client_key, client.rate, client.burst, tx);
+        } else {
+            throw new Error(`Invalid rate limiting algorithm: ${client.algorithm}`);
+        }
+    });
+};
---
+++ src/services/slidingWindow/slidingWindowService.js
@@ -0,0 +1,31 @@
+import * as requestLogRepository from "../repositories/requestLogRepository.js"
+export const checkLimit = async (client_key, rate, burst, tx) => {
+    const now = Date.now()
+    const windowStart = new Date(now.getTime() - burst * 1000)
+    //counting no of active rq in current window
+    const requestCount = await requestLogRepository.countInWindow(client_key, windowStart, tx)
+    const allowed = requestCount < rate
+    const remaining = allowed ? rate - requestCount - 1 : 0
+    if (allowed) {
+        // Request log add karein aur purane logs prune karein
+        await requestLogRepository.add(client_key, tx);
+        await requestLogRepository.clearOldLogs(client_key, windowStart, tx);
+    }
+    // Next slot kab open hoga (Reset time)
+    let resetTime = Math.ceil(now.getTime() / 1000);
+    const oldestRequest = await tx.request_logs.findFirst({
+        where: { client_key: client_key, request_time: { gte: windowStart } },
+        orderBy: { request_time: 'asc' }
+    });
+
+    if (oldestRequest) {
+        resetTime = Math.ceil(new Date(oldestRequest.request_time).getTime() / 1000 + burst);
+    }
+    return {
+        allowed,
+        remaining,
+        limit: rate,
+        reset: resetTime
+    };
+};
+
